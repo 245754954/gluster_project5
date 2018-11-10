719f4f8210cb9eb7756026a29fc87b98bb06baa5
lychuan <lychuan.cs@gmail.com>
2018-11-10 10:36:37 +0800

    Switch from JSONObject to alibaba.fastjson Modify ModulationTree in interfaces request to modify the server side correspondingly

---
+++ hycloud-client/src/main/java/cn/edu/nudt/hycloudclient/justfortrial/ListNode.java
@@ -0,0 +1,71 @@
+package cn.edu.nudt.hycloudclient.justfortrial;
+
+import cn.edu.nudt.hycloudinterface.entity.ModulationTree;
+import cn.edu.nudt.hycloudinterface.entity.Node;
+import cn.edu.nudt.hycloudinterface.entity.utils.helper;
+import com.alibaba.fastjson.JSON;
+
+import java.math.BigInteger;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListNode {
+    List<Node> mNodes;
+
+    public ListNode(){
+        mNodes = new ArrayList<Node>();
+    }
+
+    public ListNode(int num){
+        mNodes = new ArrayList<Node>();
+        SecureRandom sr = new SecureRandom();
+        for (int i = 0; i < num; i++) {
+            BigInteger bg = new BigInteger(160, sr);
+            this.mNodes.add(new Node(bg, 1));
+        }
+    }
+
+    public List<Node> getmNodes() {
+        return mNodes;
+    }
+
+    public void setmNodes(List<Node> mNodes) {
+        this.mNodes = mNodes;
+    }
+
+    public void dump(){
+        for(Node nd: this.mNodes){
+            helper.print("getmModulator:" + nd.getmModulator());
+            helper.print("getmStatus:" + nd.getmStatus());
+        }
+    }
+
+    public  static  void main(String ... argv){
+//        ListNode ln = new ListNode(5);
+//        ln.dump();
+//
+////        JSONObject jsonObject = JSONObject.fromObject(ln);
+////        helper.print(jsonObject.toString());
+////
+////        ListNode restoreLN = (ListNode) JSONObject.toBean(jsonObject, ListNode.class);
+////        restoreLN.dump();
+//
+//        String lnstring = JSON.toJSONString(ln);
+//        helper.print("lnstring: " + lnstring);
+//
+//        ListNode restoreLN = JSON.parseObject(lnstring, ListNode.class);
+//        restoreLN.dump();
+
+        ModulationTree tree = new ModulationTree(4);
+        tree.dump();
+
+        String treestr = JSON.toJSONString(tree);
+        helper.print(treestr);
+
+        ModulationTree restoretree = JSON.parseObject(treestr, ModulationTree.class);
+        restoretree.dump();
+
+
+    }
+}
--- hycloud-server/src/main/java/cn/edu/nudt/hycloudserver/controller/TestController.java
+++ hycloud-server/src/main/java/cn/edu/nudt/hycloudserver/controller/TestController.java
@@ -1,7 +1,7 @@
 package cn.edu.nudt.hycloudserver.controller;
 
-import cn.edu.nudt.hycloudserver.entity.StudentServer;
-import net.sf.json.JSONObject;
+import cn.edu.nudt.hycloudinterface.entity.ModulationTree;
+import com.alibaba.fastjson.JSON;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +15,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @RequestMapping(value = "/add", method = {RequestMethod.GET, RequestMethod.POST})
-    public JSONObject saveModulation(String student1, String student2) {
+//    public JSONObject saveModulation(String student1, String student2) {
+    public void saveModulation(String modulationTree) {
+
+        System.out.println("recv segmentList: " + modulationTree);
+        ModulationTree restoreTree = JSON.parseObject(modulationTree, ModulationTree.class);
+        restoreTree.dump();
+
+//        JSONObject jsonObject = JSONObject.fromObject(segmentList);
+//        SegmentList recvList = (SegmentList) JSONObject.toBean(jsonObject, SegmentList.class);
+//        for (int i = 0; i < recvList.size(); i++){
+//            helper.print("i: " + recvList.get(i));
+//        }
+
+//            System.out.println("recv modulationTree: " + modulationTree);
+//        JSONObject jsonObject = JSONObject.fromObject(modulationTree);
+//        ModulationTree recvTree = (ModulationTree) JSONObject.toBean(jsonObject, ModulationTree.class);
+//        recvTree.dump();
+
+
+
 //        ModulationTreeServer mo = new ModulationTreeServer();
 //        List<NodeServer> list = new ArrayList<>();
 //        NodeServer node1 = new NodeServer();
@@ -47,22 +66,26 @@ public class TestController {
 //            return "failed";
 //        }
 
-        System.out.println("student1" + student1);
-        System.out.println("student2" + student2);
-        JSONObject jsonObject1 = JSONObject.fromObject(student1);
-        JSONObject jsonObject2 = JSONObject.fromObject(student2);
-        StudentServer studentx1 = (StudentServer) JSONObject.toBean(jsonObject1, StudentServer.class);
-        StudentServer studentx2 = (StudentServer) JSONObject.toBean(jsonObject2, StudentServer.class);
+//        System.out.println("student1" + student1);
+//        System.out.println("student2" + student2);
+//        JSONObject jsonObject1 = JSONObject.fromObject(student1);
+//        JSONObject jsonObject2 = JSONObject.fromObject(student2);
+//        StudentServer studentx1 = (StudentServer) JSONObject.toBean(jsonObject1, StudentServer.class);
+//        StudentServer studentx2 = (StudentServer) JSONObject.toBean(jsonObject2, StudentServer.class);
+//
+//
+//        System.out.println("xxxxx"+studentx1.toString());
+//        System.out.println("xxxxx"+studentx2.toString());
+//        //System.out.println("hhhhh"+student.getName());
+//
+//
+//        studentx1.setName("haskjdhflkjahs");
+//        return JSONObject.fromObject(studentx1);
 
-
-        System.out.println("xxxxx"+studentx1.toString());
-        System.out.println("xxxxx"+studentx2.toString());
-        //System.out.println("hhhhh"+student.getName());
-
-
-        studentx1.setName("haskjdhflkjahs");
-        return JSONObject.fromObject(studentx1);
-
-
+//        System.out.println("recv filename: " + filename);
+//        JSONObject jsonObject = JSONObject.fromObject(filename);
+//        String recvfilename = (String) JSONObject.toBean(jsonObject, String.class);
+//
+//        System.out.println("reconstruct filename: " + filename);
     }
 }
