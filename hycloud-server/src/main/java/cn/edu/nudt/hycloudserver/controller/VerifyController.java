dee92de96757b1635e8dec931516e3e31ff67cad
245754954 <245754954@qq.com>
2018-11-13 13:34:27 +0800

    zfc

--- hycloud-server/src/main/java/cn/edu/nudt/hycloudserver/controller/VerifyController.java
+++ hycloud-server/src/main/java/cn/edu/nudt/hycloudserver/controller/VerifyController.java
@@ -3,8 +3,11 @@ package cn.edu.nudt.hycloudserver.controller;
 import cn.edu.nudt.hycloudinterface.entity.BlockInfo;
 import com.alibaba.fastjson.JSON;
 
+@RestController
+@RequestMapping("/versify")
 public class VerifyController {
 
+    @RequestMapping(value = "/verifyBlock")
     public Boolean verifyBlock(String filename, String blockIdx){
         Boolean rv = false;
         // query the status of blockIdx in filename
